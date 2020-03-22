5a4656b23963d1b3608bf463946e371236350109
gbundala <gabrielbundala@gmail.com>
2020-03-22 19:38:18 +0300

    updating strip integration and updating our title (favicon file did not exist :(

--- src/pages/checkout/checkout.js
+++ src/pages/checkout/checkout.js
@@ -8,6 +8,8 @@ import { createStructuredSelector } from 'reselect';
 
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button';
+
 const CheckoutPage = ({ cartItems, total }) => (
 	<div className='checkout-page'>
 		<div className='checkout-header'>
@@ -43,6 +45,14 @@ const CheckoutPage = ({ cartItems, total }) => (
 		<div className='total'>
 			<span>TOTAL: ${total}</span>
 		</div>
+
+		<div className='test-warning'>
+			*Please use the following test credit card for payment
+			<br />
+			4242 4242 4242 4242 - Exp: 12/20 - CVV: 123
+		</div>
+
+		<StripeCheckoutButton price={total} />
 
 	</div>
 );
